b1080df09286c1d4d9570ed72dbeba41a6bca956
Nurzhigit <Nurzhigit00a@gmail.com>
2024-02-17 17:34:53 +0600

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://sb.ecobnb.net/app/uploads/sites/3/2020/01/nature-1170x490.jpg' />
+            </div>
+            <div>ava + descr</div>
+            <div>My posts</div>
+        </div>
+    )
+}
+
+export default Profile;
